57943afecdbee9c60fab0dcbdd7c0ca260eb0cf7
Tushar Sharma <tushar123cse@gmail.com>
2020-04-22 02:08:21 +0530

    RestTemplate configured to use GSON converter

--- CallerMicroservice/src/main/java/com/optimisation/CallerMicroservice/CallerMicroserviceApplication.java
+++ CallerMicroservice/src/main/java/com/optimisation/CallerMicroservice/CallerMicroserviceApplication.java
@@ -1,8 +1,14 @@
 package com.optimisation.CallerMicroservice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -14,7 +20,13 @@ public class CallerMicroserviceApplication {
 
 	@Bean
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		GsonHttpMessageConverter gsonConverter = new GsonHttpMessageConverter();
+		
+		List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
+		
+		messageConverters.add(gsonConverter);
+		
+		return new RestTemplate(messageConverters);
 	}
 
 }
